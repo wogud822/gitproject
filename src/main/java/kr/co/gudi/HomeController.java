6c23b46022c02753a6300e4f50b6167799da6bea
lim_jae_hyeong <wogud822@gmail.com>
2023-10-30 14:36:18 +0900

    최초 프로젝트 업로드

---
+++ src/main/java/kr/co/gudi/HomeController.java
@@ -0,0 +1,26 @@
+package kr.co.gudi;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		model.addAttribute("msg", "환영합니다");
+		logger.info("index");
+		return "home";
+	}
+	
+}
